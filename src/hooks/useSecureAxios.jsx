679885d62797efd465106251ad54b1608a04eada
CyberXCore <209958457+CyberXCore@users.noreply.github.com>
2026-02-08 04:10:50 +0600

    Initial commit

---
+++ src/hooks/useSecureAxios.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+const instance = axios.create({
+  baseURL: "https://nexira-server.vercel.app",
+});
+
+const useSecureAxios = () => {
+  useEffect(() => {
+    const responseInterceptor = instance.interceptors.request.use((config) => {
+      config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
+
+      return config;
+    });
+
+    return () => {
+      instance.interceptors.request.eject(responseInterceptor);
+    };
+  }, []);
+
+  return instance;
+};
+
+export default useSecureAxios;
